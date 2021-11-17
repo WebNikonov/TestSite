b3b5580f96677893f5f5048b37f77290e6729541
Nikonov Dmitriy <40035751+WebNikonov@users.noreply.github.com>
2021-11-17 17:11:34 +0700

    Add files via upload
    Два года назад был тут в последний раз. Мои и того скудные навыки по вэбу улетучились и теперь мне нужно их восполнять с нуля.

---
+++ TestSite/index.php
@@ -0,0 +1,73 @@
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Your mr.Present</title>
+
+     <link rel="stylesheet" href="style.css">
+     <link rel="stylesheet" href="slick-1.8.1/slick/slick.css">
+     
+</head>
+ <body>
+ <section>    
+ <div class="contaner">
+    <div class="header-menu">
+
+        
+    </div>
+
+    <div class="slider">
+        <div class="slide">
+
+        </div>
+    </div>
+
+
+    <div class="products"> 
+        <div class="product-item">
+            <img src="img/gucci.png">
+            <div class="product-list">
+            <h3>Маленькое черное платье</h3>
+            <span class="price">₽ 1999</span>
+            <a href="" class="button">В корзину</a>
+            </div>
+        </div>
+
+
+        <div class="product-item">
+            <img src="img/louis.jpg">
+            <div class="product-list">
+            <h3>Маленькое черное платье</h3>
+            <span class="price">₽ 1999</span>
+            <a href="" class="button">В корзину</a>
+            </div>
+        </div>
+
+        <div class="product-item">
+            <img src="https://html5book.ru/wp-content/uploads/2015/10/black-dress.jpg">
+            <div class="product-list">
+            <h3>Маленькое черное платье</h3>
+            <span class="price">₽ 1999</span>
+            <a href="" class="button">В корзину</a>
+            </div>
+        </div>
+
+
+       
+    </div>
+
+
+
+    </div>
+
+ </section>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="sclick-1.8.1/slick/slick.js"></script>
+ </body>
+ 
+ </html>
+
